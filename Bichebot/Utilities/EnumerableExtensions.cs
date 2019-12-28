adc43a4ffa767e3016946273b4364b98d5847f67
Belianin <belyaninwork@gmail.com>
2019-12-28 22:45:29 +0500

    Delete obsolete extensions

--- Bichebot/Utilities/EnumerableExtensions.cs
+++ Bichebot/Utilities/EnumerableExtensions.cs
@@ -4,7 +4,6 @@ using System.Linq;
 
 namespace Bichebot.Utilities
 {
-    [Obsolete]
     public static class EnumerableExtensions
     {
         public static IEnumerable<T> ForEach<T>(this IEnumerable<T> source, Action<T> action)
@@ -20,21 +19,6 @@ namespace Bichebot.Utilities
         public static bool ContainsAny(this string row, IEnumerable<string> strings)
         {
             return strings.Any(row.Contains);
-        }
-        
-        public static T RandomReadonly<T>(this IReadOnlyCollection<T> source, Random random)
-        {
-            return source.ElementAt(random.Next(0, source.Count));
-        }
-        
-        public static T Random<T>(this ICollection<T> source, Random random)
-        {
-            return random.Choose(source);
-        }
-        
-        public static T Random<T>(this IEnumerable<T> source, Random random, Func<T, int> weight)
-        {
-            return random.Choose(source, weight);
         }
     }
 }
