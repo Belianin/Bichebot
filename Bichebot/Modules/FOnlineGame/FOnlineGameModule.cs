a381b590e3f0c1c86440ed7a621e58d8c889aa3e
Belianin <belyaninwork@gmail.com>
2020-01-05 15:19:03 +0500

    FOnline init

--- Bichebot/Modules/FOnlineGame/FOnlineGameModule.cs
+++ Bichebot/Modules/FOnlineGame/FOnlineGameModule.cs
@@ -1,13 +1,24 @@
+using System.Threading.Tasks;
 using Bichebot.Core;
 using Bichebot.Modules.Base;
+using Discord.WebSocket;
 
 namespace Bichebot.Modules.FOnlineGame
 {
     public class FOnlineGameModule : StatefulBaseModule<Character>
     {
-        public FOnlineGameModule(IBotCore core) : base(core, () => new Character())
+        private readonly FOnlineGameModuleSettings settings;
+        public FOnlineGameModule(IBotCore core, FOnlineGameModuleSettings settings)
+            : base(core, () => new Character())
         {
-            
+            this.settings = settings;
+        }
+
+        protected override async Task HandleMessageAsync(SocketMessage message)
+        {
+            if (message.Channel.Id != settings.GameChannelId)
+                return;
+            return;
         }
     }
 }
---
+++ Bichebot/Modules/FOnlineGame/FOnlineGameModuleSettings.cs
@@ -0,0 +1,7 @@
+namespace Bichebot.Modules.FOnlineGame
+{
+    public class FOnlineGameModuleSettings
+    {
+        public ulong GameChannelId { get; set; }
+    }
+}
